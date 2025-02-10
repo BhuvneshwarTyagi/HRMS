a003a6003c9a32ef74279210f4814c91701e7a17
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 <bhanu68tyagi@gmail.com>
2025-02-10 23:50:27 +0530

    final commit

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://hrms-733x.onrender.com';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://hrms-733x.onrender.com';
+// const BASE_URL = 'http://localhost:8000';
 
 
 export { BASE_URL};
